37ee10f03b754e7f141daa3ddcd9655cfdb3a977
Ludwig <lkdev314@gmail.com>
2023-02-21 17:32:36 +0100

    Generalizes analysis method to prepare for more data

--- evaluator/src/main/scala/Main.scala
+++ evaluator/src/main/scala/Main.scala
@@ -28,9 +28,17 @@ import forsyde.io.java.typed.viewers.decision.results.AnalyzedActor
   var handler = ForSyDeModelHandler()
   for p <- Files.list(data_source).collect(Collectors.toList()).asScala do
     val graph: ForSyDeSystemGraph = handler.loadModel(p)
-    println(p.getFileName().toString() + ": " + analyze(graph).get)
+    println(p.getFileName().toString() + " " + analyze(graph).get)
 
   println("END")
+
+class AnalysisResult(
+      numericalThroughput: Option[Double],
+      exactThroughput: Option[Double]):
+  def numThroughput = if numericalThroughput.isDefined then numericalThroughput.get else Double.NaN
+  def exaThroughput = if exactThroughput.isDefined then exactThroughput.get else Double.NaN
+  override def toString(): String =
+    numThroughput + " " + exactThroughput
 
 def getDoubleProp(a: Vertex, name: String): Option[Double] =
   var prop: Option[VertexProperty] = a.getProperties().get(name) match
@@ -41,13 +49,19 @@ def getDoubleProp(a: Vertex, name: String): Option[Double] =
   catch
     case e: ClassCastException => None
 
-def getThroughput(a: Vertex): Option[Double] =
+def analyze(graph: ForSyDeSystemGraph): Option[AnalysisResult] =
+  // finds all actors with throughput property, and extracts numerical throughput
+  val numRes = graph.vertexSet().stream().map(getNumThroughput(_)).filter(_.isDefined)
+      .map(_.get).collect(Collectors.toList()).asScala.reduceLeftOption(_ min _)
+  val exaRes = None
+  numRes match
+    case n: Some[Double] => Some(AnalysisResult(numRes, exaRes))
+    case _ => None
+
+def getNumThroughput(a: Vertex): Option[Double] =
   val jopt = AnalyzedActor.safeCast(a).map(al => {
     al.getThroughputInSecsNumerator().toDouble/al.getThroughputInSecsDenominator().toDouble
   })
   if jopt.isPresent() then Some(jopt.get()) else None
 
-def analyze(graph: ForSyDeSystemGraph): Option[Double] =
-  // finds all actors with throughput property
-  graph.vertexSet().stream().map(getThroughput(_)).filter(_.isDefined).map(_.get)
-      .collect(Collectors.toList()).asScala.reduceLeftOption(_ min _)
+//def getExactThroughput()
