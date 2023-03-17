b9bca798577f96bb395c8a8d14fbed6b182ef52b
Ludwig Karlsson <ludwigka@kth.se>
2023-03-17 18:10:33 +0100

    Changes behavior to pipe both outputs to the same log file.

--- experiments/run-nohup.sh
+++ experiments/run-nohup.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 stdoutfile=${1}.nohup.out
-stderrfile=${1}.nohup.err
 
 case $# in
     1)
-	nohup ./run.sh $1 1>$stdoutfile 2>$stderrfile &
+	nohup ./run.sh $1 1>$stdoutfile 2>&1 &
 	;;
     2)
-	nohup ./run.sh $1 $2 1>$stdoutfile 2>$stderrfile &
+	nohup ./run.sh $1 $2 1>$stdoutfile 2>&1 &
 	;;
     3)
-	nohup ./run.sh $1 $2 $3 1>$stdoutfile 2>$stderrfile &
+	nohup ./run.sh $1 $2 $3 1>$stdoutfile 2>&1 &
 	;;
     4)
-	nohup ./run.sh $1 $2 $3 $4 1>$stdoutfile 2>$stderrfile &
+	nohup ./run.sh $1 $2 $3 $4 1>$stdoutfile 2>&1 &
 	;;
     *)
 	echo "Incorrect number of arguments! Please specify 1-4 arguments!"
