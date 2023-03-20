b70cfec0bb74c3dbf2782e95cb09f277f192045b
Ludwig <lkdev314@gmail.com>
2023-03-20 11:14:29 +0100

    Adds help to w2u

--- experiments/w2u.sh
+++ experiments/w2u.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+# removes carriage returns from line breaks.
+# (Who even thought that computers were typewriters?)
+
+if [ $# -eq 0 ]
+then
+    echo "Usage: w2u.sh <target>"
+    exit 0
+fi
+
 sed -i.bak 's/\r$//g' $1
 echo "DONE"
